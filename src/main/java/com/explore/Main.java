2bed341ab48cf98ab74a2de0b133aa32ea6d037a
pancham01 <pancham9557@gmail.com>
2025-06-01 11:59:20 +0530

    update

--- src/main/java/com/explore/Main.java
+++ src/main/java/com/explore/Main.java
@@ -1,22 +1,35 @@
 package com.explore;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import com.explore.dao.EmployeeDao;
 import com.explore.dao.EmployeeDaoImpl;
 import com.explore.entity.Employee;
 
 public class Main {
+	
+	
 
 	public static void main(String[] args) throws SQLException {
 		
 		EmployeeDao edao = new EmployeeDaoImpl();
 
-		Employee ankit = new Employee(12, "Ankit", "N/A", 77777);
+//		Employee ankit = new Employee(14 , "Lokesh", "N/A", 77777);
 		
-//		edao.saveEmp(shiv);
+//		edao.saveEmp(ankit);
 		
-		edao.updateEmp(ankit);
+//		edao.updateEmp(ankit);
+		
+		
+//		edao.printAllEmp();
+		
+		List<Employee> allEmp = edao.getAllEmp();
+		
+		for(Employee e : allEmp)
+		{
+			System.out.println(e);
+		}
 
 		
 	}
--- src/main/java/com/explore/dao/EmployeeDaoImpl.java
+++ src/main/java/com/explore/dao/EmployeeDaoImpl.java
@@ -2,23 +2,32 @@ package com.explore.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import com.explore.entity.Employee;
+import com.mysql.cj.jdbc.Driver;
 
 public class EmployeeDaoImpl implements EmployeeDao {
 
 	private static final String INSERT_QUERY = "INSERT INTO EMPLOYEE VALUES (%d,'%s','%s',%d)";
 	private static final String UPDATE_QUERY = "UPDATE EMPLOYEE SET NAME = '%s', GENDER = '%s', SALARY = %d WHERE ID = %d";
+	private static final String SELECT_QUERY = "SELECT * FROM EMPLOYEE";
 
 	private static Connection conn = null;
 
 	static {
 		try {
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/testDb", "root", "root");
+//			Driver d = new Driver();
+//			Properties prop = new Properties();
+//			prop.put("user", "root");
+//			prop.put("password", "root");
+//			conn = d.connect("jdbc:mysql://localhost:3306/testDb",prop);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -56,18 +65,43 @@ public class EmployeeDaoImpl implements EmployeeDao {
 	@Override
 	public List<Employee> getAllEmp() {
 		List<Employee> list = new ArrayList<Employee>();
-		
-		
-		
-		list.add(null);
-		
-		
+
+		try (Statement statement = conn.createStatement()) {
+
+			ResultSet rs = statement.executeQuery(SELECT_QUERY);
+
+			while (rs.next()) {
+				Employee employee = new Employee();
+
+				employee.setId(rs.getInt(1));
+				employee.setName(rs.getString(2));
+				employee.setGender(rs.getString(3));
+				employee.setSalary(rs.getInt(4));
+				list.add(employee);
+			}
+
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+		}
+
 		return list;
 	}
 
 	@Override
 	public void printAllEmp() {
-		// TODO Auto-generated method stub
+
+		try (Statement statement = conn.createStatement()) {
+
+			ResultSet rs = statement.executeQuery(SELECT_QUERY);
+
+			while (rs.next()) {
+				System.out.println("ID = " + rs.getInt(1) + "\t NAME = " + rs.getString(2) + "\t Gender = "
+						+ rs.getString(3) + "\t Salary = " + rs.getInt(4));
+			}
+
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+		}
 
 	}
 
